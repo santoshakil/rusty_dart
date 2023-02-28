36b846f0b5e6b587306db6a3a13e1899b383d3f3
Santo Shakil <shakil.mezbah@gmail.com>
2023-02-28 10:51:34 +0600

    Copy Dir

--- lib/lib.dart
+++ lib/lib.dart
@@ -1,1 +1,43 @@
+import 'dart:ffi' as ffi;
+import 'dart:io' show Platform;
+import 'package:ffi/ffi.dart' as pffi;
+
 void execRust() {}
+
+late ffi.DynamicLibrary dl;
+typedef CopyDirFunc = ffi.Int32 Function(
+  ffi.Pointer<ffi.Char> src,
+  ffi.Pointer<ffi.Char> dst,
+);
+typedef CopyDir = int Function(
+  ffi.Pointer<ffi.Char> src,
+  ffi.Pointer<ffi.Char> dst,
+);
+
+const libPath = '/Users/agmacone/Projects/Dart/rusty_dart/libmain.so';
+const source = '/Users/agmacone/Projects/Dart/rusty_dart/dummy/source';
+const destination = '/Users/agmacone/Projects/Dart/rusty_dart/dummy/dest';
+
+void main() {
+  if (Platform.isMacOS || Platform.isLinux) {
+    dl = ffi.DynamicLibrary.open(libPath);
+  } else if (Platform.isWindows) {
+    dl = ffi.DynamicLibrary.open("rust.dll");
+  } else {
+    print('Unsupported platform');
+    return;
+  }
+
+  final copyDirPointer = dl.lookup<ffi.NativeFunction<CopyDirFunc>>('copy_dir');
+  final copyDir = copyDirPointer.asFunction<CopyDir>();
+
+  final src = source.toNativeUtf8();
+  final dst = destination.toNativeUtf8();
+
+  final result = copyDir(src.cast<ffi.Char>(), dst.cast<ffi.Char>());
+
+  pffi.calloc.free(src);
+  pffi.calloc.free(dst);
+
+  print('Copy result: $result');
+}
--- rusty_dart_lib/src/main.rs
+++ rusty_dart_lib/src/main.rs
@@ -1,10 +1,37 @@
+use std::{
+    ffi::{c_char, c_int, CStr, CString},
+    fs,
+    path::Path,
+};
+
 fn main() {
-    let a = 1;
-    let b = 2;
-    let c = add(a, b);
-    println!("{} + {} = {}", a, b, c);
+    let source = "/Users/agmacone/Projects/Dart/rusty_dart/dummy/source";
+    let destination = "/Users/agmacone/Projects/Dart/rusty_dart/dummy/dest";
+
+    let source_cstring = CString::new(source).unwrap();
+    let destination_cstring = CString::new(destination).unwrap();
+
+    copy_dir(source_cstring.as_ptr(), destination_cstring.as_ptr());
 }
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
+#[no_mangle]
+pub extern "C" fn copy_dir(src: *const c_char, dst: *const c_char) -> c_int {
+    let source = Path::new(unsafe { CStr::from_ptr(src).to_str().unwrap() });
+    let destination = Path::new(unsafe { CStr::from_ptr(dst).to_str().unwrap() });
+    if source.is_dir() {
+        fs::create_dir_all(destination).unwrap();
+        for entry in fs::read_dir(source).unwrap() {
+            let entry = entry.unwrap();
+            let entry_path = entry.path();
+            let dest_path = destination.join(entry.file_name());
+            if entry_path.is_dir() {
+                fs::create_dir_all(&entry_path).unwrap();
+            } else {
+                fs::copy(&entry_path, &dest_path).unwrap();
+            }
+        }
+    } else {
+        fs::copy(source, destination).unwrap();
+    }
+    0
 }
