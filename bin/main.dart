afa186dd76dd09e0db790e086ef787b3b2296131
Santo Shakil <shakil.mezbah@gmail.com>
2023-10-18 18:23:21 +0600

    init

--- bin/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:rusty_dart/lib.dart';
-
-void main(List<String> arguments) {
-  execRust();
-}
---
+++ bin/rusty_dart.dart
@@ -0,0 +1,5 @@
+import 'package:rusty_dart/rusty_dart.dart' as rusty_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rusty_dart.calculate()}!');
+}
